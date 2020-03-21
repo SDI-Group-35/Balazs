dafe27d313726170327b7f34059a0ccc73b18bc7
Balazs Szilagyi <n0812461@ntu.ac.uk>
2020-03-21 14:37:42 +0000

    Delet node v2
    Added changes to deletNode

--- pixelmap.cpp
+++ pixelmap.cpp
@@ -93,7 +93,28 @@ void coordList::addNode(int addData){
     }
 }
 
-/* Initialize deleteNode function */
+/* Define deleteNode function */
 void coordList::deleteNode(int delData){
-
+   nodePtr delPtr = NULL;
+   temp = head;
+   curr = head;
+   while (curr != NULL && curr->data != delData)    /* if we went throught the list and did not find the data to be deleted, we need to exit the loop */
+   {                                                /* also we need to make sure the data is not the data we need to delete, otherwise exit the loop */
+     temp = curr;                                   /* temp is 1 step behinde curr*/
+     curr == curr->next;                            /* curr one step ahead of temp, since it points to next node in list */
+                                                    /* when the while loop exit one of the 2 conditions failed, either no data found or data found to be deleted */
+   }
+   if(curr == NULL)                                 /* If it is equal to NULL we did not find the data to be deleted */
+   {
+       cout << delData << "Value was not in the list\n";    /* Display if the data was not found to be deleted */
+       delete delPtr;                               /* delPtr needs to be deleted, otherwise it will folat in memory */
+   }
+   else                                             /* Node was found with matching data */
+   {
+       delPtr = curr;                               /* delPtr becamoes equal to the current node */
+       curr = curr->next;                           /* advance current to the next node, it does not have to get involed with delete */
+       temp->next = curr;                           /* since temp is one step behind compared to current, we can patch the list together again by patching temp and jumping over node to be deleted and adding patching it to actual curr*/
+       delete delPtr;                               /* delete whatever is delPtr is pointing to */
+       cout <<" The deleted data was "<< delData<< " and got deleted\n";
+   }
 }
